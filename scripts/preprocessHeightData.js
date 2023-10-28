f95e29f4e9e50047a84bc06808b8df95f08a0d83
jpiccirillo <piccirilloj1@gmail.com>
2023-10-28 10:56:12 -0500

    Format files for clearer diff in later work

--- scripts/preprocessHeightData.js
+++ scripts/preprocessHeightData.js
@@ -1,73 +1,75 @@
-const csv = require('csvtojson')
-const csvFilePath = './original-height-data.csv'
+const csv = require("csvtojson");
+const csvFilePath = "./original-height-data.csv";
 const {
   writePreprocessedData,
   getDatesBetween,
   getSpeciesDisplayName,
-} = require('./utils')
+} = require("./utils");
 
-const splitDate = (date) => date
+const splitDate = (date) => date;
 const standardizeUnits = (a) =>
-  a.Unit === 'inches' ? Number(a.Height) : Number(a.Height / 2.54)
+  a.Unit === "inches" ? Number(a.Height) : Number(a.Height / 2.54);
 
 csv()
   .fromFile(csvFilePath)
   .then(preprocessData)
   .then((content) => {
-    writePreprocessedData(content.finalData, 'processed-height-data')
-    writePreprocessedData(content.fullDateSet, 'full-date-set')
-  })
+    writePreprocessedData(content.finalData, "processed-height-data");
+    writePreprocessedData(content.fullDateSet, "full-date-set");
+  });
 
 async function preprocessData(csvData) {
-  const sortedData = csvData.sort((a, b) => new Date(a.Date) - new Date(b.Date))
+  const sortedData = csvData.sort(
+    (a, b) => new Date(a.Date) - new Date(b.Date)
+  );
 
   var dates = [
     splitDate(sortedData[0].Date),
     splitDate(sortedData[sortedData.length - 1].Date),
-  ]
+  ];
 
   let fullDateSet = getDatesBetween(
     new Date(dates[0]).getTime(),
-    new Date(dates[dates.length - 1]).getTime(),
-  )
+    new Date(dates[dates.length - 1]).getTime()
+  );
 
-  let mapIndexedByPlant = {}
+  let mapIndexedByPlant = {};
   for (let recording of csvData) {
     if (!(recording.Name in mapIndexedByPlant)) {
-      mapIndexedByPlant[recording.Name] = []
+      mapIndexedByPlant[recording.Name] = [];
     }
-    mapIndexedByPlant[recording.Name].push(recording)
+    mapIndexedByPlant[recording.Name].push(recording);
   }
 
   // Combine available plant recordings with full date set, so that every
   // plant has a baseline of the full dates.  Dates for which there was no
   // recording are null
-  let mapWithBlankDays = {}
+  let mapWithBlankDays = {};
   for (let plantName in mapIndexedByPlant) {
-    let plantRecordings = mapIndexedByPlant[plantName]
+    let plantRecordings = mapIndexedByPlant[plantName];
     let paddedPlantRecordings = Array.from(fullDateSet).map((date) => {
-      let dateEntry = { date, value: null }
-      let format = (b) => new Date(b).toString()
+      let dateEntry = { date, value: null };
+      let format = (b) => new Date(b).toString();
       for (let a = 0; a < plantRecordings.length; a++) {
         if (format(plantRecordings[a].Date) === format(date)) {
-          return { ...dateEntry, value: standardizeUnits(plantRecordings[a]) }
+          return { ...dateEntry, value: standardizeUnits(plantRecordings[a]) };
         }
       }
-      return dateEntry
-    })
-    mapWithBlankDays[plantName] = paddedPlantRecordings.map((a) => a.value)
+      return dateEntry;
+    });
+    mapWithBlankDays[plantName] = paddedPlantRecordings.map((a) => a.value);
   }
 
   // Now finally prepare the data the way c3.js expects
-  let finalData = []
+  let finalData = [];
   for (let plantName in mapWithBlankDays) {
-    let data = mapWithBlankDays[plantName]
+    let data = mapWithBlankDays[plantName];
     finalData.push({
       title: plantName,
       species: getSpeciesDisplayName(plantName),
       data,
-    })
+    });
   }
 
-  return { finalData, fullDateSet }
+  return { finalData, fullDateSet };
 }
--- src/components/LineChart/helper.js
+++ src/components/LineChart/helper.js
@@ -1,59 +1,59 @@
-import * as d3 from 'd3'
-import c3 from 'c3'
-import tippy from 'tippy.js'
+import * as d3 from "d3";
+import c3 from "c3";
+import tippy from "tippy.js";
 import {
   mergeDeep,
   createAxisGroup,
   formatDate,
   formatValue,
   longMonthNames,
-} from '../../utils/'
-import staticProps from './staticProps.json'
+} from "../../utils/";
+import staticProps from "./staticProps.json";
 
-const getNode = (a) => document.getElementById(getTippyId(a))
-let axisGroup
+const getNode = (a) => document.getElementById(getTippyId(a));
+let axisGroup;
 
 export function ChartFactory(_data, { colors, xScale, margin, svgID }) {
   const mergedProps = mergeDeep(staticProps, {
     oninit: function () {
-      this.svg.attr('id', svgID)
-      axisGroup = createAxisGroup(d3.select(`#${svgID}`), this.height)
+      this.svg.attr("id", svgID);
+      axisGroup = createAxisGroup(d3.select(`#${svgID}`), this.height);
     },
     onrendered: function () {
-      let { height, width, orgXDomain } = this
+      let { height, width, orgXDomain } = this;
       const x = xScale
         .domain(orgXDomain.map((a) => new Date(a)))
-        .range([margin.left - margin.laneGutter, width - margin.right])
+        .range([margin.left - margin.laneGutter, width - margin.right]);
 
       axisGroup
         .transition()
         .duration(0)
-        .attr('transform', `translate(0, ${height + 5})`)
+        .attr("transform", `translate(0, ${height + 5})`)
         .call(
           d3.axisBottom(x).tickFormat((one) => {
-            const label = x.tickFormat()(one)
+            const label = x.tickFormat()(one);
             return longMonthNames.includes(label)
-              ? d3.timeFormat('%b')(one)
-              : label
-          }),
+              ? d3.timeFormat("%b")(one)
+              : label;
+          })
         )
-        .selectAll('text')
-        .attr('y', 10)
-        .attr('x', -8)
-        .attr('dy', '0em')
-        .attr('transform', 'rotate(-45)')
-        .style('text-anchor', 'end')
+        .selectAll("text")
+        .attr("y", 10)
+        .attr("x", -8)
+        .attr("dy", "0em")
+        .attr("transform", "rotate(-45)")
+        .style("text-anchor", "end");
     },
     data: {
-      x: 'x',
-      xFormat: '%m/%d/%Y', // 'xFormat' can be used as custom format of 'x'
+      x: "x",
+      xFormat: "%m/%d/%Y", // 'xFormat' can be used as custom format of 'x'
       columns: _data,
       colors,
       onmouseover: function (a) {
-        if (getNode(a)) getNode(a)._tippy.show()
+        if (getNode(a)) getNode(a)._tippy.show();
       },
       onmouseout: function (a) {
-        if (getNode(a)) getNode(a)._tippy.hide()
+        if (getNode(a)) getNode(a)._tippy.hide();
       },
     },
     size: {
@@ -66,32 +66,32 @@ export function ChartFactory(_data, { colors, xScale, margin, svgID }) {
         },
       },
       x: {
-        type: 'timeseries',
+        type: "timeseries",
         tick: {
           format: formatDate,
         },
       },
     },
-  })
-  return c3.generate(mergedProps)
+  });
+  return c3.generate(mergedProps);
 }
 
 export function getTippyId(data) {
-  let { id, index } = data
-  return `${id.replace(/[ ]/g, '')}-${index}`
+  let { id, index } = data;
+  return `${id.replace(/[ ]/g, "")}-${index}`;
 }
 
 export function setUpTooltips() {
   // First step - get selector on all dots on the page
   // Then augment these dots with a specified ID, data-tippy-content string
-  for (let dot of document.getElementsByClassName('c3-circle')) {
-    let { id, value, x: date } = dot.__data__
+  for (let dot of document.getElementsByClassName("c3-circle")) {
+    let { id, value, x: date } = dot.__data__;
     if (value !== null) {
-      let label = `${id}: ${formatValue(value)}in tall on ${formatDate(date)}`
-      let uid = getTippyId(dot.__data__)
-      dot.setAttribute('data-tippy-content', label)
-      dot.setAttribute('id', uid)
+      let label = `${id}: ${formatValue(value)}in tall on ${formatDate(date)}`;
+      let uid = getTippyId(dot.__data__);
+      dot.setAttribute("data-tippy-content", label);
+      dot.setAttribute("id", uid);
     }
   }
-  tippy('svg circle')
+  tippy("svg circle");
 }
